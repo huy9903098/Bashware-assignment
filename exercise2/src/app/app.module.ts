fe502f2698ad8981f8171c2511d157f56d7eae5e
Huy <skyzerzlol@gmail.com>
2021-05-14 21:08:04 +0300

    clean more code

--- exercise2/src/app/app.module.ts
+++ exercise2/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CustomFormFieldComponent } from './custom-form-field/custom-form-field.component';
+
 import { FormDisplayComponent } from './form-display/form-display.component';
 
 @NgModule({
-  declarations: [AppComponent, CustomFormFieldComponent, FormDisplayComponent],
+  declarations: [AppComponent, FormDisplayComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
