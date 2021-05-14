b02c4d7838ecdee58bdea2b24700adb199f13f24
Huy <skyzerzlol@gmail.com>
2021-05-14 13:42:09 +0300

    start exercise 2

---
+++ exercise2/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CustomFormFieldComponent } from './custom-form-field/custom-form-field.component';
+
+@NgModule({
+  declarations: [AppComponent, CustomFormFieldComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
